2b79ed9909e5761fb21fac990c4b225b031b407a
Madalina <madalina_serban01@yahoo.com>
2022-07-27 23:11:01 +0300

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,84 @@
+interface PersonInterface {
+    public boolean isOver18();
+    public boolean licenseIsValid();
+}
+
+public class Person implements PersonInterface {
+   String personal_identification_number;
+    String first_name;
+    String last_name;
+    String date_of_birth;
+    char gender; //f sau m
+    short emited_year_of_driving_license;
+
+    public Person(String personal_identification_number, String first_name, String last_name, String date_of_birth, char gender, short emited_year_of_driving_license) {
+        this.personal_identification_number = personal_identification_number;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.date_of_birth = date_of_birth;
+        this.gender = gender;
+        this.emited_year_of_driving_license = emited_year_of_driving_license;
+    }
+
+    //setters
+
+    public void setPersonal_identification_number(String personal_identification_number) {
+        this.personal_identification_number = personal_identification_number;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setDate_of_birth(String date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public void setEmited_year_of_driving_license(short emited_year_of_driving_license) {
+        this.emited_year_of_driving_license = emited_year_of_driving_license;
+    }
+
+    //getters
+    public String getPersonal_identification_number() {
+        return personal_identification_number;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public short getEmited_year_of_driving_license() {
+        return emited_year_of_driving_license;
+    }
+
+    @Override
+    public boolean isOver18() {
+        return false;
+    }
+
+    @Override
+    public boolean licenseIsValid() {
+        return false;
+    }
+}
+
---
+++ src/Vehicle.java
@@ -0,0 +1,65 @@
+interface VehicleInterface {
+ public boolean manufacturingYearIsValid();
+ public boolean registration_numberIsValid();
+}
+
+public class Vehicle implements  VehicleInterface
+{
+ String registration_number;
+ int manufacturing_year;
+ String brand;
+ String colour;
+ enum type {HATCHBACK,
+  SEDAN,
+  SUV}
+
+
+ @Override
+ public boolean manufacturingYearIsValid() {
+  return false;
+ }
+
+ @Override
+ public boolean registration_numberIsValid() { //regex
+  return false;
+ }
+
+ public String getRegistration_number() {
+  return registration_number;
+ }
+
+ public int getManufacturing_year() {
+  return manufacturing_year;
+ }
+
+ public String getBrand() {
+  return brand;
+ }
+
+ public String getColour() {
+  return colour;
+ }
+
+ public Vehicle(String registration_number, int manufacturing_year, String brand, String colour) {
+  this.registration_number = registration_number;
+  this.manufacturing_year = manufacturing_year;
+  this.brand = brand;
+  this.colour = colour;
+ }
+
+ public void setRegistration_number(String registration_number) {
+  this.registration_number = registration_number;
+ }
+
+ public void setManufacturing_year(int manufacturing_year) {
+  this.manufacturing_year = manufacturing_year;
+ }
+
+ public void setBrand(String brand) {
+  this.brand = brand;
+ }
+
+ public void setColour(String colour) {
+  this.colour = colour;
+ }
+}
