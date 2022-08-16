26c8611ff6adbfb331d776f4b3da1f3e4fe2f034
Madalina <madalina_serban01@yahoo.com>
2022-08-16 18:05:37 +0300

    insert rent obj into db

--- src/Main.java
+++ src/Main.java
@@ -10,32 +10,30 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException
     {          List<Vehicle> vehiclesList = new ArrayList<Vehicle>();
         List<Person> personList = new ArrayList<Person>();
+        List<Rented> rentedList=new ArrayList<Rented>();
         StringTokenizer token=null;
-        String personal_identification_number;
-        String first_name;
-        String last_name;
-        String date_of_birth;
-        String gender;
-        int emited_year_of_driving_license;
-        File file=new File("C:/Cursuri Siemens/Java/VehicleRentingApp/persoane.txt");
+        String date_start;
+        String date_finish;
+        String kilometeters;
+        String u_id;
+        String licence;
+        File file=new File("C:/Cursuri Siemens/Java/VehicleRentingApp/inchirieri.txt");
         Scanner scan=new Scanner(file);
         while(scan.hasNextLine())
         { token=new StringTokenizer(scan.nextLine(),", ");
-            personal_identification_number= token.nextToken();
-            last_name= token.nextToken();
-            first_name=token.nextToken();
-            gender=token.nextToken();
-            emited_year_of_driving_license=Integer.parseInt(token.nextToken());
-
-
+            date_start= token.nextToken();
+            date_finish= token.nextToken();
+            kilometeters=token.nextToken();
+            u_id=token.nextToken();
+            licence= token.nextToken();
             try{
         Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","Mailo");
         Statement statement=connection.createStatement();
-        statement.executeUpdate("insert into clients" + "(`personal_identification_number`,\n" +
-                "`last_name`,\n"+
-                "`first_name`,\n" +
-                "`gender`,\n" +
-                "`emited_year_of_driving_license`)"+" "+"values ("+"'"+personal_identification_number+"'"+","+"'"+last_name+"'"+", "+"'"+first_name+"'"+", "+"'"+gender+"',"+emited_year_of_driving_license+")");
+        statement.executeUpdate("insert into rented" +
+                "(`date_start`,\n" +
+                "`date_end`,\n" +
+                "`kilometeters`,\n"  +"`FK_cnp`,\n" +"`FK_registration_number`"+ ")"+" "
+                +"values ("+"'"+date_start+"'"+","+"'"+date_finish+"'"+","+"'"+kilometeters+"'"+", "+"'"+u_id+"','"+licence+"')");
 
     }catch (Exception e)
     {
